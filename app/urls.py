1d86d44700b8614c9e813210006a46b783df16c5
Sreeshma-S <sreeprofid@gmail.com>
2022-03-14 01:00:31 +0530

    new

--- app/urls.py
+++ app/urls.py
@@ -1,22 +1,9 @@
-# from django.conf.urls import url
-# from .api import ListApi, CardApi
-# from django.views.generic import TemplateView
-#
-# urlpatterns = [
-#     url(r'^lists$', ListApi.as_view()),
-#     url(r'^cards$', CardApi.as_view()),
-#     url(r'^home', TemplateView.as_view(template_name="app/home1.html")),
-# ]
+from django.urls import path, re_path
+from . import views
+from .views import *
 
 
-
-
-# generates whole url mapping
-from .api import ListViewSet, CardViewSet
-from rest_framework.routers import DefaultRouter
-
-router = DefaultRouter()
-router.register(r'lists', ListViewSet)
-router.register(r'cards', CardViewSet)
-
-urlpatterns = router.urls
+urlpatterns = [
+    path('', views.home, name = "home"),
+    # path('media', views.media, name='media'),
+]
